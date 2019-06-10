a8abcd43bc5e20f341ec6dd0f901eeaf93004f16
Tone <t.proum@gmail.com>
2019-06-10 21:13:45 +0200

    feat(timekeeper): add duration selector

--- NeoPixel.ino
+++ NeoPixel.ino
@@ -3,8 +3,11 @@
 
 #define LED_PIN 13
 #define RESTART_BUTTON_PIN 15
+#define SELECTOR_BUTTON 14
 #define LED_COUNT 16
 #define BRIGHTNESS 20
+#define CONFERENCE_DURATION 3000
+#define QUICKY_DURATION 1200
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_KHZ800 + NEO_GRB);
 
@@ -12,15 +15,17 @@ class TimeKeeper
 {
 private:
   int totalTime, remainingTime;
+  bool started;
 
 public:
   TimeKeeper(int seconds)
   {
     this->totalTime = seconds;
     this->remainingTime = seconds;
+    this->started = false;
   }
   /**
-   * Retrieve the totalTime set for this timeKeeper
+   * Retrieve the totalTime set for this timeKeeper.
    */
   int getTotalTime() const
   {
@@ -36,6 +41,27 @@ public:
   }
 
   /**
+   * Start the TimeKeeper.
+   */
+  void start() {
+    this->started = true;
+  }
+
+  /**
+   * Stop the TimeKeeper.
+   */
+  void stop() {
+    this->started = false;
+  }
+
+  /**
+   * Is the TimeKeeper started ? 
+   */
+  bool isStarted(){
+    return this->started;
+  }
+
+  /**
    * wait for the given number of seconds.
    */
   void wait(int seconds)
@@ -45,13 +71,46 @@ public:
   }
 };
 
+class TimeSelector{
+  private:
+    int index;
+    int PRESETS [2] = {CONFERENCE_DURATION, QUICKY_DURATION};
 
-TimeKeeper timeKeeper = TimeKeeper(32);
+  public:  
+    TimeSelector(){
+      this->index = 0;
+    }
+
+    /**
+     * Switch to next preset.
+     */
+    void next(){
+      this->index=(this->index+1) % 2; 
+    }
+
+    /**
+     * Get preset value.
+     */
+    int getPreset(){
+      return this->PRESETS[this->index];
+    }
+
+
+};
+
+TimeSelector timeSelector = TimeSelector();
+TimeKeeper timeKeeper = TimeKeeper(timeSelector.getPreset());
 
 void restart() {
-  timeKeeper = TimeKeeper(32);
+  timeKeeper.stop();
+  timeKeeper = TimeKeeper(timeSelector.getPreset());
+  timeKeeper.start();
 }
 
+void select() {
+  timeKeeper.stop();
+  timeSelector.next();
+}
 
 void setup()
 {
@@ -60,6 +119,8 @@ void setup()
   strip.setBrightness(BRIGHTNESS); // Set BRIGHTNESS to about 1/5 (max = 255)
   pinMode(RESTART_BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(RESTART_BUTTON_PIN, restart, FALLING);
+  pinMode(SELECTOR_BUTTON, INPUT_PULLUP);
+  attachInterrupt(SELECTOR_BUTTON, select, FALLING);
 }
 
 void loop()
@@ -67,28 +128,42 @@ void loop()
   display();
 }
 
-
 void display() {
-  while (timeKeeper.getRemainingTime() > 0)
-  {
-    log_printf("New loop is starting with %d \n", timeKeeper.getRemainingTime());
-    displayTimeKeeper(timeKeeper);
-  }
-  display();
+  log_printf("time keeper is started %b \n", timeKeeper.isStarted());
+    while (timeKeeper.getRemainingTime() > 0 && timeKeeper.isStarted())
+    {
+      log_printf("New loop is starting with %d \n", timeKeeper.getRemainingTime());
+      displayTimeKeeper();
+    }
+    log_printf("Selector with %d \n", timeSelector.getPreset());
+    displayTimeSelector();
 }
 
-void displayTimeKeeper(TimeKeeper& timeKeeper)
-{
-  for (int seconds = strip.numPixels() - 1; seconds >= 0; seconds--)
+void displayTimeSelector(){
+  strip.clear();
+  int coeff = (CONFERENCE_DURATION / timeSelector.getPreset());
+  for (int i = 0; i < int((strip.numPixels()) / coeff); i++)
   {
-    displayRemainingTime(timeKeeper);
+    strip.setPixelColor(i, strip.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
+  }
+  strip.show();
+  delay(1000);
+}
+
+void displayTimeKeeper()
+{
+  int seconds = strip.numPixels() - 1;
+  while (seconds >= 0 && timeKeeper.isStarted())
+  {
+    displayRemainingTime();
     strip.setPixelColor(seconds, strip.Color(BRIGHTNESS, BRIGHTNESS, BRIGHTNESS));
     strip.show();
     timeKeeper.wait(1);
+    seconds--;
   }
 }
 
-void displayRemainingTime(TimeKeeper& timeKeeper)
+void displayRemainingTime()
 {
   strip.clear();
   int coeff = (timeKeeper.getTotalTime() * 10 / timeKeeper.getRemainingTime());
